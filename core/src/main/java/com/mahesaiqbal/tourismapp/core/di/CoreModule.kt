2cc230b351bf7b2b53b9e97e16f6e3b0e20ed390
Iqbal <iqbalridwansyah.ir@gmail.com>
2020-11-21 01:42:52 +0700

    Add SQLCipher for database encryption

--- core/src/main/java/com/mahesaiqbal/tourismapp/core/di/CoreModule.kt
+++ core/src/main/java/com/mahesaiqbal/tourismapp/core/di/CoreModule.kt
@@ -8,6 +8,8 @@ import com.mahesaiqbal.tourismapp.core.data.source.remote.RemoteDataSource
 import com.mahesaiqbal.tourismapp.core.data.source.remote.network.ApiService
 import com.mahesaiqbal.tourismapp.core.domain.repository.ITourismRepository
 import com.mahesaiqbal.tourismapp.core.utils.AppExecutors
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.android.ext.koin.androidContext
@@ -19,10 +21,15 @@ import java.util.concurrent.TimeUnit
 val databaseModule = module {
     factory { get<TourismDatabase>().tourismDao() }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("dicoding".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             TourismDatabase::class.java, "Tourism.db"
-        ).fallbackToDestructiveMigration().build()
+        )
+        .fallbackToDestructiveMigration()
+        .openHelperFactory(factory)
+        .build()
     }
 }
 
